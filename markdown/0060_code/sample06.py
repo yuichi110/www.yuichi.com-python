fa0d2af8b2fcd0f14219d74024b46011565fcd28
yuichi ito <yito86@gmail.com>
2018-01-19 10:02:57 +0900

    a

---
+++ markdown/0060_code/sample06.py
@@ -0,0 +1,5 @@
+a = 100
+if 10 > a:
+  print('True: 10 > a')
+else:
+  print('False: 10 > a')
---
+++ markdown/0060_code/sample07.py
@@ -0,0 +1,7 @@
+a = 70
+if 80 <= a:
+  print('Pass, Good')
+elif 60 <= a:
+  print('Pass')
+else:
+  print('Fail')
